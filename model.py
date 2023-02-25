71968a006872784e0aa33dcee2011512760b6d50
Erik Connerty <erikc@email.sc.edu>
2023-02-25 14:10:49 -0500

    stuff

--- model.py
+++ model.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import random as rand
 n = 3
 
-matrix = np.array([[1.0, .5,1.0],[1.0, 1.0,1.0],[0.0,1.0,0.0]]) #Trust scores
+# Todo: Import agents from a csv with their trust scores
+matrix = np.array([[1.0, .21,.43],[.47, .44,.33],[0.0,.21,.20]]) #Trust scores
 
-agents = np.vstack(np.array([-1.0, 1.0, 0.0])) #Initial stance on opinion
+agents = np.vstack(np.array([-.9, .65, -.124])) #Initial stance on opinion
 
 tick = 1 #The first tick is considered the initial state
-growth_rate = 1
+growth_rate = 1.000
 
 
 curr_agents = agents[0:].copy()
@@ -27,12 +28,12 @@ while(True):
             if(i == j):
                 continue #The agent should not update its opinion on itself
             sum += (matrix[i,j]*((curr_agents[j]-curr_agents[i])))/growth_rate
-        next_agents[i] = (curr_agents[i] + (sum/(n)))
+        next_agents[i] = (curr_agents[i] + (sum/(n-1)))
     agents = np.append(agents,next_agents,axis=1)
     tick+=1
     curr_agents = np.vstack(agents[:,tick-1])
     
-    growth_rate = growth_rate+growth_rate #idk why or how i decided this was right
+    growth_rate = growth_rate*growth_rate #idk why or how i decided this was right
     
     time.sleep(.5)
     
