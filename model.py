9ac154cec89a6856339d793495efa651f55f5f5d
Erik Connerty <erikc@email.sc.edu>
2023-02-24 20:48:37 -0500

    initial commit

---
+++ model.py
@@ -0,0 +1,33 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+n = 2
+
+matrix = np.matrix('1 .2; 1 1') #Trust scores
+
+#agents = [np.array([.8]),np.array([.2])] #Initial stance on opinion
+
+tick = 1 #The first tick is considered the initial state
+
+
+curr_agents = agents
+
+while(True):
+    print(agents)
+    for i in range(n):
+        for j in range(n):
+            if(i == j):
+                continue #The agent should not update its opinion on itself
+            curr_agents[i] = ((matrix[i,j]*curr_agents[j])+((curr_agents[i]*tick)))/(tick+1)
+    agents = np.vstack(np.append(agents,np.vstack(curr_agents)))
+    tick+=1
+    
+    #Plot the data
+    plt.plot(agents)
+    plt.show(block=False)
+    
+    while(True):
+        userInput = input('Press Space')
+        if (userInput == ' '):
+            break
+    
