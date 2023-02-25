a6472b4929f1d725b955e12c5a7394da69dc8791
Erik Connerty <erikc@email.sc.edu>
2023-02-25 02:11:42 -0500

    off by a lot

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import random as rand
 n = 2
 
-matrix = np.array([[1.0, 1.0],[1.0, 1.0]]) #Trust scores
+matrix = np.array([[1.0, 1.0],[.1, 1.0]]) #Trust scores
 
 agents = np.vstack(np.array([1.0, -1.0])) #Initial stance on opinion
 
@@ -14,6 +14,7 @@ tick = 1 #The first tick is considered the initial state
 curr_agents = agents[0:].copy()
 while(True):
     next_agents = curr_agents.copy()
+    print(next_agents)
     #Plot the data
     for i in range(n):
         plt.plot(agents[i])
@@ -24,7 +25,7 @@ while(True):
             if(i == j):
                 continue #The agent should not update its opinion on itself
             sum += matrix[i,j]*curr_agents[j]
-        next_agents[i] = curr_agents[i] + sum
+        next_agents[i] = (curr_agents[i] + (sum/(tick)))
     agents = np.append(agents,next_agents,axis=1)
     tick+=1
     curr_agents = np.vstack(agents[:,tick-1])
