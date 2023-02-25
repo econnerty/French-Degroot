73e387fc236763175aa0ce00c64bd89b0584c5dc
Erik Connerty <erikc@email.sc.edu>
2023-02-24 21:10:07 -0500

    idk

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import random as rand
 n = 2
 
-matrix = np.matrix('1 .5; .2 1') #Trust scores
+matrix = np.matrix('1 .5; 1 1') #Trust scores
 
 agents = [np.array([.8,.1])] #Initial stance on opinion
 
@@ -16,8 +17,10 @@ while(True):
 
     for i in range(n):
         for j in range(n):
+
             if(i == j):
                 continue #The agent should not update its opinion on itself
+            #if(rand.random() < matrix[i,j]):
             curr_agents[i] = ((matrix[i,j]*curr_agents[j])+((curr_agents[i]*tick)))/(tick+1) #This is not right
     agents.append(curr_agents)
     tick+=1
@@ -27,7 +30,7 @@ while(True):
     plt.show(block=False)
     
     while(True):
-        userInput = input('Press Space')
+        userInput = input()
         if (userInput == ' '):
             break
     
