075c2fa864fc77dff58f80613fa8ca5cff117b0c
Erik Connerty <erikc@email.sc.edu>
2023-02-24 20:58:51 -0500

    note

--- model.py
+++ model.py
@@ -3,23 +3,23 @@ import time
 import matplotlib.pyplot as plt
 n = 2
 
-matrix = np.matrix('1 .2; 1 1') #Trust scores
+matrix = np.matrix('1 .5; .2 1') #Trust scores
 
-#agents = [np.array([.8]),np.array([.2])] #Initial stance on opinion
+agents = [np.array([.8,.1])] #Initial stance on opinion
 
 tick = 1 #The first tick is considered the initial state
 
 
-curr_agents = agents
+curr_agents = agents[0]
 
 while(True):
-    print(agents)
+
     for i in range(n):
         for j in range(n):
             if(i == j):
                 continue #The agent should not update its opinion on itself
-            curr_agents[i] = ((matrix[i,j]*curr_agents[j])+((curr_agents[i]*tick)))/(tick+1)
-    agents = np.vstack(np.append(agents,np.vstack(curr_agents)))
+            curr_agents[i] = ((matrix[i,j]*curr_agents[j])+((curr_agents[i]*tick)))/(tick+1) #This is not right
+    agents.append(curr_agents)
     tick+=1
     
     #Plot the data
