41da204d7c6f1563212fa6cfb233246c0126b905
Erik Connerty <erikc@email.sc.edu>
2023-02-24 21:51:57 -0500

    still not right

--- model.py
+++ model.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import random as rand
 n = 2
 
-matrix = np.matrix('1 .5; 1 1') #Trust scores
+matrix = np.matrix('1 .1 .2; .9 1 .3;1 1 1') #Trust scores
 
-agents = [np.array([.8,.1])] #Initial stance on opinion
+agents = [np.array([-1,1,.5])] #Initial stance on opinion
 
 tick = 1 #The first tick is considered the initial state
 
@@ -15,19 +15,21 @@ curr_agents = agents[0]
 
 while(True):
 
+    #Plot the data
+    plt.plot(agents)
+    plt.show(block=False)
     for i in range(n):
+        sum = 0
         for j in range(n):
 
             if(i == j):
                 continue #The agent should not update its opinion on itself
             #if(rand.random() < matrix[i,j]):
-            curr_agents[i] = ((matrix[i,j]*curr_agents[j])+((curr_agents[i]*tick)))/(tick+1) #This is not right
+            sum += matrix[i,j]*curr_agents[j] #This is not right
+        print(sum/(n*tick))
+        curr_agents[i] = curr_agents[i] + sum/(n*tick)
     agents.append(curr_agents)
     tick+=1
-    
-    #Plot the data
-    plt.plot(agents)
-    plt.show(block=False)
     
     while(True):
         userInput = input()
